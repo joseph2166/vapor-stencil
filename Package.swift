91e86d1ca91168d9db50c928f3f61c56631fb02e
Joe Thomson <joseph_2166@mac.com>
2026-01-30 20:49:21 +0000

    Fix for missing product in Package.swift

--- Package.swift
+++ Package.swift
@@ -3,16 +3,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "VaporStencil",
+    name: "vapor-stencil",
     platforms: [
             .macOS(.v10_15),
             .iOS(.v13),
             .tvOS(.v13),
             .watchOS(.v6)
         ],
+    products: [
+        .library(name: "vapor-stencil", targets: ["vapor-stencil"])
+    ],
     dependencies: [
         .package(url: "https://github.com/kylef/Stencil.git", from: "0.15.1"),
-        .package(url: "https://github.com/tannernelson/vapor.git", from: "2.0.0")
+        .package(url: "https://github.com/tannernelson/vapor.git", from: "4.121.1")
     ],
     targets: [
         .target(name: "vapor-stencil", dependencies: ["Stencil", .product(name: "Vapor", package: "vapor")], path: "Sources")
