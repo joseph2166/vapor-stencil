61f4caad2a9f3ef6e4afc31864e28f8466d728b2
Joe Thomson <joseph_2166@mac.com>
2026-01-30 16:45:56 +0000

    Updated Package.swift to swift-tools-version:6.0

--- Package.swift
+++ Package.swift
@@ -1,9 +1,21 @@
+// swift-tools-version:6.0
+
 import PackageDescription
 
 let package = Package(
     name: "VaporStencil",
+    platforms: [
+            .macOS(.v10_15),
+            .iOS(.v13),
+            .tvOS(.v13),
+            .watchOS(.v6)
+        ],
     dependencies: [
-        .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0),
-        .Package(url: "https://github.com/tannernelson/vapor.git", majorVersion: 0)
-    ]
+        .package(url: "https://github.com/kylef/Stencil.git", from: "0.15.1"),
+        .package(url: "https://github.com/tannernelson/vapor.git", from: "2.0.0")
+    ],
+    targets: [
+        .target(name: "vapor-stencil", dependencies: ["Stencil", .product(name: "Vapor", package: "vapor")], path: "Sources")
+    ],
+    swiftLanguageModes: [.v6]
 )
